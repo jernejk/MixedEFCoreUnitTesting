5f3caa9638792c0df763dda72e7350914326efed
Jernej Kavka (JK) <jernej.kavka@outlook.com>
2018-02-19 19:58:35 +1000

    Added another simple unit test to test if global filter.

--- MixedDbUnitTests.Tests/SimpleTests.cs
+++ MixedDbUnitTests.Tests/SimpleTests.cs
@@ -9,18 +9,43 @@ namespace MixedDbUnitTests.Tests
         [Fact]
         public void ShouldBeAbleToAddAndGetEntity()
         {
+            // Prepare
             var context = GetDbContext();
             context.TestDatas.Add(new TestData
             {
                 Data = "Test"
             });
-
             context.SaveChanges();
 
+            // Execute
             var data = context.TestDatas.ToList();
 
+            // Assert
             Assert.Single(data);
             Assert.Contains(data, d => d.Data == "Test");
+        }
+
+        [Fact]
+        public void ShouldBeAbleToIgnoreDeletedEntity()
+        {
+            // Prepare
+            var context = GetDbContext();
+            context.TestDatas.Add(new TestData
+            {
+                Data = "Deleted",
+                IsDeleted = true
+            });
+            context.TestDatas.Add(new TestData
+            {
+                Data = "Test",
+                IsDeleted = false
+            });
+            context.SaveChanges();
+
+            // Execute
+            var data = context.TestDatas.ToList();
+            Assert.Single(data);
+            Assert.All(data, d => Assert.False(d.IsDeleted));
         }
     }
 }
